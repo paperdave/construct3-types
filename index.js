f51690e52922edb38d0c8d108e0471a56c0659e8
dave <dave@davecode.me>
2021-10-03 15:59:30 -0400

    fix worker mode

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-if (typeof window !== 'undefined' && typeof module !== 'undefined' && window.C3) {
+if (window.C3) {
   module.exports = {
     ...window,
     C3Event: window.C3.Event,
   };
 } else {
-  throw new Error('You cannot use this module in Node.JS or the browser, but only within Construct 3 Games.');
+  throw new Error(
+    "You cannot use this module in Node.JS or the browser, but only within Construct 3 Games."
+  );
 }
